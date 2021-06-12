5b358c7d08f5b3a5bf221fff597b96cfd1273f0f
Ana Martinez <ana.martinez.040816@gmail.com>
2021-06-12 18:56:46 -0500

    iniciolibrerias

---
+++ gonethp/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Logomobile } from './Components/logomobile/Logomobile';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1>holy</h1>     
+        </div>
+      </header>
+      <Logomobile />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ gonethp/src/Components/logomobile/Logomobile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import '../logomobile/Logomobile.css';
+
+
+export const Logomobile = () => {
+  return (
+    <div className="path14">
+      <a>H</a>
+    </div>
+  );
+}
